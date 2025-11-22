eb83526f0cf1c52caaa7a5359a08da0ddcb84b2e
Khen chandarapisey <piseykhenchandara93@gmail.com>
2025-11-23 02:46:08 +0700

    modify import path , in ordertable

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+
 app.use('/api/category', categoryRoutes);
 app.use('/api/brand', brandRoutes);
 app.use('/api/checkout', checkoutRoute);
--- frontend/src/seller/components/orderManagement/ordersTable/OrdersTableOnMb.jsx
+++ frontend/src/seller/components/orderManagement/ordersTable/OrdersTableOnMb.jsx
@@ -21,7 +21,7 @@ const OrdersTableOnMb = ({
             className="bg-white shadow-md rounded-lg border border-gray-200 p-4 hover:shadow-xl transition relative"
           >
             <button 
-              onClick={() => {
+              onClick={() => {s
                 setOpen(true)
                 fetchTheReceipt(order.user_id, order.order_number)
               }}
