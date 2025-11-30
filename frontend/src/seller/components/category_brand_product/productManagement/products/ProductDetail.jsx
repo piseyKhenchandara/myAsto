9ba9f887d97fc4e481f6887e8f33aa45a2567609
Khen chandarapisey <piseykhenchandara93@gmail.com>
2025-11-30 18:58:14 +0700

    make produtdetail ui better

--- frontend/src/seller/components/category_brand_product/productManagement/products/ProductDetail.jsx
+++ frontend/src/seller/components/category_brand_product/productManagement/products/ProductDetail.jsx
@@ -79,7 +79,7 @@ const ProductDetail = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12'>
                 
                 {/* Image Section */}
-                <section className='space-y-4 md:sticky md:top-12'>
+                <section className='space-y-4 md:sticky md:top-20 md:self-start'>
                     {/* Main Image */}
                     <figure className='overflow-hidden h-[200px] md:h-[350px]'>
                         {selectedImage ? (
