99781330cebfbeba4c0a8db16faa9299090a0223
Khen chandarapisey <piseykhenchandara93@gmail.com>
2025-12-19 02:36:03 +0700

    modify autheticate.js

--- backend/middleware/autheticate.js
+++ backend/middleware/autheticate.js
@@ -1,32 +1,30 @@
-export const authenticate = async (req, res, next) => {
-    try {
+import db from "../models/index.js";
+import jwt, { decode } from 'jsonwebtoken';
+
+
+export const authenticate = async (req,res, next) => {
+    try{
         const bearerToken = req.headers.authorization?.startsWith('Bearer')
-            ? req.headers.authorization.split(' ')[1] : null;
+        ? req.headers.authorization.split(' ')[1] : null;
 
         const token = bearerToken || req.cookies?.token;
-        if (!token) {
-            return res.status(401).json({ message: "Authentication required!" });
+        if(!token) {
+            return res.status(401).json({message : "Authetication required !"});
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
       
-        const user = await db.User.findByPk(decoded.id);
-        
-        if (!user) {
-            return res.status(401).json({ message: "User not found!" });
-        }
 
-   
-        req.user = {
-            id: user.id,
-            role: user.role,
-            email: user.email
-        };
-        
+        req.user = {   
+            id: decoded.id, 
+            role: decoded.role, 
+           
+}       ; 
         next();
 
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
+
+    }  
+    catch(error){
+        return res.status(500).json({message : error.message});
     }
-}
+} 
