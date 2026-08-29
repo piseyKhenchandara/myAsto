7d3ab75c0c26a07c3910be4ae2ae033366084f15
Khen chandarapisey <piseykhenchandara93@gmail.com>
2026-08-30 01:28:57 +0700

    security: remove sensitive files and fix firebase config

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -18,4 +18,3 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-auth.settings.appVerificationDisabledForTesting = true;
